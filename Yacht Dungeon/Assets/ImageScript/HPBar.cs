f7a984f0395b66ababeac6105d8847c61a720808
Jinsu Oh <51368746+Laid41798175@users.noreply.github.com>
2022-05-15 20:51:38 +0900

    220515
    Image uploaded
    Creating Scene

---
+++ Yacht Dungeon/Assets/ImageScript/HPBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour {
+
+    [SerializeField] Sprite fullSprite;
+    [SerializeField] Sprite emptySprite;
+
+    GameObject[] hpBars;
+    GameObject hpBar2;
+    GameObject hpBar3;
+    GameObject hpBar4;
+    GameObject hpBar5;
+
+    public void Start() {
+        hpBars = new GameObject[5];
+        for (int i = 1; i <= 5; i++) {
+            hpBars[i] = transform.Find("HPBarEmpty" + i.ToString()).gameObject;
+        }
+    }
+
+    public void UpdateHP(int hp) {
+        for (int i = 0; i < hp; i++) {
+            hpBars[i].GetComponent<SpriteRenderer>().sprite = fullSprite;
+        }
+        for (int i = hp; i < 5; i++) {
+            hpBars[i].GetComponent<SpriteRenderer>().sprite = emptySprite;
+        }
+    }
+}
